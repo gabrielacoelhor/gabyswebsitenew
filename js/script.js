ad10081ce7b41174611ad837c4c7b4a9e63a184f
gabrielacoelhor <gabrielacoelhoreis06@gmail.com>
2025-05-30 10:59:54 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+//header transparente
+window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header");
+    if (window.scrollY > 50) {
+      header.classList.add("transparent");
+    } else {
+      header.classList.remove("transparent");
+    }
+  });
+
+//mudar pra p&b
 const toggleBtn = document.getElementById('toggle-theme');
 toggleBtn.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
